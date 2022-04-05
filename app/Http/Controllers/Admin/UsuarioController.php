6b6972fd765a82e7c0b9400689f0667f82b8ad09
AllanRanier <allanranier18@gmail.com>
2022-04-05 18:15:29 -0300

    cadastro de ususario

--- app/Http/Controllers/Admin/UsuarioController.php
+++ app/Http/Controllers/Admin/UsuarioController.php
@@ -5,6 +5,11 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rules;
+use Illuminate\Auth\Events\Registered;
+
 
 class UsuarioController extends Controller
 {
@@ -28,18 +33,36 @@ class UsuarioController extends Controller
      */
     public function create()
     {
-
+        return view('admin.usuario.create');
     }
 
     /**
-     * Store a newly created resource in storage.
+     * Handle an incoming registration request.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\RedirectResponse
+     *
+     * @throws \Illuminate\Validation\ValidationException
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password' => ['required', 'confirmed', Rules\Password::defaults()],
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        event(new Registered($user));
+
+        Auth::login($user);
+
+        // return redirect(RouteServiceProvider::HOME);
     }
 
     /**
